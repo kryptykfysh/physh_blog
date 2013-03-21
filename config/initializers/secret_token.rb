b16679b8cea40d0c877713da12eb974755c7b07f
Kryptykfysh <kryptykfysh@kryptykfysh.co.uk>
2013-03-22 10:09:29 +1100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhyshBlog::Application.config.secret_token = '15376221fb3b8ee1c3469447c4f1a373e4682589bf117c38692fa352eff4ae8b35ad61e10f5295518a7f9d556bed9b3da0048637584c780fc0661fe30aa8a835'
